5c1797cc45bc2bdb2a1e9804a4ef98787358cff7
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2014-12-27 23:15:13 +0200

    all adds routing and controller

--- scripts/app.js
+++ scripts/app.js
@@ -4,11 +4,16 @@ var app = angular.module('adsApp', ['ngResource', 'ngRoute'])
     .config(function ($routeProvider) {
         $routeProvider
             .when('/login', {
-            templateUrl: 'views/login.html'
-        })
+                templateUrl: 'views/login.html'
+            })
             .when('/register', {
-            templateUrl: 'views/register.html'
-        });
+                templateUrl: 'views/register.html'
+            })
+            .when('/ads', {
+                templateUrl: 'views/allAds.html',
+                controller: 'AllAdsController'
+            })
+            .otherwise('/ads');
     });
 
 
