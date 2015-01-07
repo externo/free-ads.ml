ed59ff0185840c4a5dced450647feb0005bd1f13
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2015-01-07 16:34:59 +0200

    fix categories to show, forget $ :(

--- js/controllers/CategoriesCtrl.js
+++ js/controllers/CategoriesCtrl.js
@@ -2,7 +2,7 @@
 
 app.controller('CategoriesCtrl', ['$scope', 'categoriesData', function($scope, categoriesData) {
 	categoriesData.getCategories()
-		.promise
+		.$promise
 		.then(function (data){
 			$scope.categories = data;
 		});
