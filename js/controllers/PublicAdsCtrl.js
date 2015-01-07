1fed8c3dd6d636d0a6b35fc181da625f072d5e7a
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2015-01-07 23:18:18 +0200

    fix paging

--- js/controllers/PublicAdsCtrl.js
+++ js/controllers/PublicAdsCtrl.js
@@ -17,14 +17,15 @@ app.controller('PublicAdsCtrl', ['$scope', 'adsData', 'filter', function($scope,
     loadPublicAds();
 
     $scope.pageChanged = function(){
-
+        filter.setPageParams({startPage: $scope.currentPage, pageSize: numItems / numPages});
+        adsData.getPublicAds(filter.getParams());
     };
 
     $scope.$on('categoryClicked', function(event, category){
-        loadPublicAds(filter.getFilterParams());
+        loadPublicAds(filter.getParams());
     });
 
     $scope.$on('townClicked', function(event, town){
-        loadPublicAds(filter.getFilterParams());
+        loadPublicAds(filter.getParams());
     });
 }]);
