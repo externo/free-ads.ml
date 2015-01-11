5f876b75b6d285e50c37e05215546b5618f91382
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2015-01-12 00:09:14 +0200

    fix

--- app/js/directives/errorImage.js
+++ app/js/directives/errorImage.js
@@ -1,11 +1,1 @@
-'use strict';
-
-app.directive('errSrc', function() {
-    return {
-        link: function(scope, element, attrs) {
-            element.bind('error', function() {
-                attrs.$set('src', attrs.errSrc);
-            });
-        }
-    }
-});
+'use strict';app.directive('errSrc', function() {    return {        link: function(scope, element, attrs) {            element.bind('error', function() {                attrs.$set('src', attrs.errSrc);            });        }    }});
