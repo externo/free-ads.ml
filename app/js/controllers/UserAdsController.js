0946d9b1ca66073aa2c1c2f4d0a5f4881b187aec
Iliya Iliev <iliya.vasilev.iliev@gmail.com>
2015-01-11 09:55:16 +0200

    user ads fix

--- app/js/controllers/UserAdsController.js
+++ app/js/controllers/UserAdsController.js
@@ -1,19 +1,10 @@
 'use strict';
 
 app.controller('UserAdsController',
-    function ($scope, $rootScope, $location, userService, authService, notifyService) {
+    function ($scope, $rootScope, $location, adsService, authService, notifyService, $resource) {
         $rootScope.pageTitle = "User ads";
 
-        $scope.userAds = function(ads) {
-            userService.getUserAds(ads,
-                function success() {
-                    notifyService.showInfo("User ads showed successfully.");
-                    $location.path("/user/ads");
-                },
-                function error(err) {
-                    notifyService.showError("User ads failed showing", err);
-                }
-            );
-        };
+        $scope.userAds = adsService.getAds($resource);
+
     }
 );
